96ef28c26a1e004e520394e757c5d795937c9457
Mikkel Schmidt <mikschm@dfds.com>
2025-11-25 10:00:50 +0100

    Added summary for IResiliencePipelineProvider

--- src/Dafda/Consuming/Interfaces/IResiliencePipelineProvider.cs
+++ src/Dafda/Consuming/Interfaces/IResiliencePipelineProvider.cs
@@ -3,14 +3,14 @@
 namespace Dafda.Consuming.Interfaces;
 
 /// <summary>
-/// 
+/// Provides resilience pipelines for message handling based on message registration configuration.
 /// </summary>
 public interface IResiliencePipelineProvider
 {
     /// <summary>
-    /// 
+    /// Gets the resilience pipeline configured for the specified message registration.
     /// </summary>
-    /// <param name="registration"></param>
-    /// <returns></returns>
+    /// <param name="registration">The message registration containing the resilience pipeline group configuration.</param>
+    /// <returns>A <see cref="ResiliencePipeline"/> configured for the message registration.</returns>
     ResiliencePipeline GetPipelineFor(MessageRegistration registration);
 }
